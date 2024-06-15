c7681690c65b1e928e3e79a3c8fcf198c7cd5764
coronagmd <coronagmdoodle@gmail.com>
2024-06-15 12:06:49 -0700

    puppy spotlights

--- index.js
+++ index.js
@@ -60,6 +60,7 @@ navToggle.addEventListener('click', () => {
   }
 });
 
+//1st slide
 const slides = document.querySelectorAll('.pic');
 const btnRight = document.querySelector('.btn-right');
 const btnLeft = document.querySelector('.btn-left');
@@ -118,3 +119,63 @@ const prevSlide = function () {
 
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', prevSlide);
+
+//2nd slide
+const slides1 = document.querySelectorAll('.pic1');
+const btnRight1 = document.querySelector('.btn-right1');
+const btnLeft1 = document.querySelector('.btn-left1');
+
+let curSlide1 = 0;
+const maxSlide1 = slides.length;
+
+const goToSlide1 = function (slide) {
+  slides1.forEach(function (s, i) {
+    const translateXNum = 80 * (i - slide);
+    let translateYNum, rotateDeg, grayscaleNum, zIndexNum, opacityNum;
+    if (translateXNum === 0) {
+      translateYNum = 0;
+      rotateDeg = 0;
+      grayscaleNum = 0;
+      zIndexNum = 1;
+      opacityNum = 100;
+    } else if (translateXNum < 0) {
+      translateYNum = 5;
+      rotateDeg = -5;
+      grayscaleNum = 1;
+      zIndexNum = 0;
+      opacityNum = 20;
+    } else {
+      translateYNum = 5;
+      rotateDeg = 5;
+      grayscaleNum = 1;
+      zIndexNum = 0;
+      opacityNum = 20;
+    }
+    s.style.transform = `translate(${translateXNum}%, ${translateYNum}%) rotate(${rotateDeg}deg)`;
+    s.style.filter = `grayscale(${grayscaleNum})`;
+    s.style.zIndex = zIndexNum;
+    s.style.opacity = `${opacityNum}%`;
+  });
+};
+goToSlide1(0);
+
+const nextSlide1 = function () {
+  if (curSlide1 === maxSlide1 - 1) {
+    curSlide1 = 0;
+  } else {
+    curSlide1++;
+  }
+  goToSlide1(curSlide1);
+};
+
+const prevSlide1 = function () {
+  if (curSlide1 === 0) {
+    curSlide1 = maxSlide1 - 1;
+  } else {
+    curSlide1--;
+  }
+  goToSlide1(curSlide1);
+};
+
+btnRight1.addEventListener('click', nextSlide1);
+btnLeft1.addEventListener('click', prevSlide1);
